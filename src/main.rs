f24c7a3a409fb0df75e87f23279aaf8983e52dc8
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-07-11 15:36:20 +1000

    add error handling and parse zip file

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
-use clap::Parser;
-use zinspect::cli::Cli;
+use std::{fs::File, process::ExitCode};
 
-fn main() {
+use clap::Parser;
+use zinspect::{cli::Cli, match_error};
+use zip::ZipArchive;
+
+fn main() -> ExitCode {
     let cli = Cli::parse();
 
-    println!("file: {:?}", cli.path());
+    let file = match_error!(File::open(cli.path()));
+    let zip = match_error!(ZipArchive::new(file));
+
+    for file in zip.file_names() {
+        println!("zip: {}", file);
+    }
+
+    ExitCode::SUCCESS
 }
---
+++ src/utils.rs
@@ -0,0 +1,44 @@
+#[macro_export]
+macro_rules! print_error {
+    ($err:expr) => {
+        use owo_colors::OwoColorize;
+
+        eprintln!("{} {}", "error:".bright_red().bold(), $err.bold());
+    };
+}
+
+#[macro_export]
+macro_rules! return_error {
+    ($err:expr) => {
+        use std::process::ExitCode;
+        use $crate::print_error;
+
+        print_error!($err);
+        return ExitCode::FAILURE;
+    };
+}
+
+#[macro_export]
+macro_rules! handle_error {
+    ($ex:expr) => {
+        if let Err(err) = $ex {
+            use $crate::return_error;
+
+            return_error!(err);
+        };
+    };
+}
+
+#[macro_export]
+macro_rules! match_error {
+    ($ex:expr) => {
+        match $ex {
+            Ok(r) => r,
+            Err(err) => {
+                use $crate::return_error;
+
+                return_error!(err);
+            }
+        }
+    };
+}
