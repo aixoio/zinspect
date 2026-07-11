ad89a6e7f6c7f589cbf496d240f9d3e2bd52bb26
David L Terpin <79969530+aixoio@users.noreply.github.com>
2026-07-11 16:37:20 +1000

    render filename

--- src/app/info.rs
+++ src/app/info.rs
@@ -1,6 +1,12 @@
 use std::fs::File;
 
-use ratatui::{buffer::Buffer, layout::Rect, widgets::Widget};
+use ratatui::{
+    buffer::Buffer,
+    layout::Rect,
+    style::Style,
+    text::{Line, Span},
+    widgets::{Block, Padding, Paragraph, Widget},
+};
 use zip::ZipArchive;
 
 pub struct InfoWidget<'a> {
@@ -15,5 +21,16 @@ impl<'a> InfoWidget<'a> {
 }
 
 impl<'a> Widget for InfoWidget<'a> {
-    fn render(self, area: Rect, buf: &mut Buffer) {}
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let block = Block::default().padding(Padding::new(4, 0, 1, 0));
+
+        let text = Line::from(vec![
+            Span::styled("Filename    ", Style::default().blue().bold()),
+            Span::raw(self.filename),
+        ]);
+
+        let text = Paragraph::new(text).block(block);
+
+        text.render(area, buf);
+    }
 }
